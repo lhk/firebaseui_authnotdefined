c332037c6973a9c4d772832cfc53ac66a378fcdd
Lars Klein <lars.klein@rwth-aachen.de>
2019-09-15 10:24:50 +0200

    simplifying code

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,27 @@ window.onload = () => {
 
     // configure firebase
     var config = {
-        // TODO: add your config here
+        // TODO: add your config
     };
     firebase.initializeApp(config);
     var ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-    // sign-in flow
-    firebase.auth().onAuthStateChanged(function (authUser) {
-        if (authUser) {
-            console.log("already logged in");
-        }
-        else {
-            var uiConfig = {
-                callbacks: {
-                    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-                        // User successfully signed in.
-                        // Return type determines whether we continue the redirect automatically
-                        // or whether we leave that to developer to handle.
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
 
-                        //ui.start('#firebaseui-auth-container', uiConfig);
-                        return false;
-                    }
-                },
-                // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-                signInFlow: 'popup',
-                signInOptions: [
-                    firebase.auth.EmailAuthProvider.PROVIDER_ID
-                ]
-            };
-            // remove other html+
-            ui.start('#firebaseui-auth-container', uiConfig);
-        }
-    })
+                //ui.start('#firebaseui-auth-container', uiConfig);
+                return false;
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
+    };
+    ui.start('#firebaseui-auth-container', uiConfig);
 }
